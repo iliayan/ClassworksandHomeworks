b597c96b88d8f60d26abc8affd5ead7d03a1d071
Deadpool 1652 <iyanovskiy.08@gmail.com>
2023-10-02 17:39:15 +0100

    MobilePhone.java

---
+++ src/Random.java
@@ -0,0 +1,10 @@
+public class Random {
+}
+
+    public static void main(String[] args) {
+        for (int i=1; i <=9; i++){
+            for(int i=1; i<10; i++){
+                System.out.println("ILLIa")
+            }
+        }
+    }
---
+++ src/RandomNumber.java
@@ -0,0 +1,13 @@
+
+import java.util.Random;
+
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int random_number.nextInt(40) - 12;
+        System.out.println(random_number);
+
+    }
+}
+
