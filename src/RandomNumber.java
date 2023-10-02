7cabea42b95fb64f4e0963944e51ccb3a656924c
Deadpool 1652 <iyanovskiy.08@gmail.com>
2023-10-02 17:39:15 +0100

    MobilePhone.java

---
+++ src/RandomNumber.java
@@ -0,0 +1,10 @@
+
+
+import java.util.Random;
+
+
+        int number = min + rnd.nextInt(max 20 - min + 1);
+
+public class RandomNumber {
+    Random rnd = new Random(System.currentTimeMillis());
+}
